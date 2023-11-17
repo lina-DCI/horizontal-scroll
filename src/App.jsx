853fd40991cd191210286139dabc76d5be319bc4
Lina Lubig <lina.lubig@dci.education>
2023-11-17 19:11:03 +0100

    first draft

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+import './App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+    
+      <h1>horizontal scroll</h1>
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+       
+      </div> */}
+     
+      <div className='container'>
+        <div className='sticky-column'>test </div>
+        <div className='scrollable-column'>scroll Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis suscipit sapiente officiis consequuntur deleniti temporibus consectetur deserunt placeat iure. Possimus quas voluptates nobis quo iure ipsum laborum laboriosam earum esse.</div>
+        <div className='sticky-column'>test </div>
+        <div className='scrollable-column'>scroll Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis suscipit sapiente officiis consequuntur deleniti temporibus consectetur deserunt placeat iure. Possimus quas voluptates nobis quo iure ipsum laborum laboriosam earum esse.</div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
